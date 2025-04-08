cb3793f5bca69cbd7e1a470946a4d7713547c96a
maddineni77 <maddinenivenkatrao1@gmail.com>
2025-04-08 16:23:25 +0530

    string methods

---
+++ str.js
@@ -0,0 +1,12 @@
+let str="maddineni"
+console.log(str.toUpperCase())
+let str2="MADDINENI"
+console.log(str2.toLowerCase())
+let str3="venkataraomaddineni"
+console.log(str3.charAt(2))
+console.log(str2.indexOf("N"))
+console.log(str2.lastIndexOf("N"))
+console.log(str3.length)
+console.log(str3.slice(0,))
+console.log(str3.includes("enk"))
+
